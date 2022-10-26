9b5a1ab3223c35b9912d2444c3ec8972c0f0bedd
Ayushinitp <116718528+Ayushinitp@users.noreply.github.com>
2022-10-26 15:38:46 +0530

    Add files via upload

---
+++ ass1.c
@@ -0,0 +1,105 @@
+//question 1
+/*
+#include<stdio.h>
+int main(){
+    printf("Hello Students");
+    return 0;
+}
+*/
+
+//question 2
+/*
+#include <stdio.h>
+int main(){
+    printf("Hello\nStudent");
+    return 0;
+}
+*/
+
+//question 3
+/*
+#include<stdio.h>
+int main(){
+    printf("\"MySirG\"");
+    return 0;
+}
+*/
+
+//question 4
+/*
+#include <stdio.h>
+int main(){
+    printf("\\n");
+    return 0;
+}
+*/
+
+//question 5
+/*
+#include <stdio.h>
+int main(){
+    printf("\\r");
+    return 0;
+}
+*/
+
+//question 6
+/*
+#include<stdio.h>
+int main(){
+    printf("\"Teacher\'s Day\"");
+    return 0;
+}
+*/
+//question 7
+/*
+#include <stdio.h>
+int main(){
+    int a ,b , c;
+    printf("Enter two numbers :");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("sum of %d and %d is %d",a,b,c);
+    return 0;
+
+}
+*/
+
+//question 8
+/*
+#include <stdio.h>
+int main(){
+    int a;
+    printf("Enter a number : ");
+    scanf("%d",&a);
+    printf("Square of %d is %d",a,a*a);
+    return 0;
+}
+*/
+
+//question 9
+/*
+#include <stdio.h>
+int main(){
+    int a,b;
+    printf("Enter length of rectangle :");
+    scanf("%d",&a);
+    printf("Enter breadth of rectangle :");
+    scanf("%d",&b);
+    printf("Area of rectangle is %d",a*b);
+    return 0;
+}
+*/
+
+//question 10
+/*
+#include <stdio.h>
+int main(){
+    float r,a;
+    printf("Enter radius of circle :");
+    scanf("%f",&r);
+    a=3.14*r*r;
+    printf("\"Area of circle is %f having the radius %f\"",a,r);
+    return 0;
+}
+*/
